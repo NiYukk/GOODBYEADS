465afd90f2ebdf2e64fb2053cccb630fdd2a5e34
YuKi <60054733+NiYukk@users.noreply.github.com>
2026-01-16 22:27:32 +0800

    Update dl.py

--- data/python/dl.py
+++ data/python/dl.py
@@ -43,7 +43,10 @@ adblock = [
     "https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-easylist.txt",
     "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
     "https://raw.githubusercontent.com/sjhgvr/oisd/main/abp_small.txt",
-    "https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/data/combined_disguised_trackers.txt"
+    "https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/data/combined_disguised_trackers.txt",
+    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/native.xiaomi.txt",
+    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/native.vivo.txt",
+    "https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt",
 ]
 
 # 白名单规则
